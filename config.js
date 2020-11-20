7b34806e57cfb8eb563f2b8516699a9c3d652232
Derek Diaz Correa <gigabit77@gmail.com>
2020-11-19 19:46:39 -0600

    Initial Commit

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports ={
+    SID :'xxx', //TWILIO SID
+    TOKEN : 'xxx', // TWILIO TOKEN
+    TO_PHONES: ['0000000000', '0000000000', '0000000000'], // List of numbers to notify
+    FROM : '0000000000', // TWILIO FROM PHONE NUMBER
+    ITEM : "https://www.amazon.com/dp/B08FC5L3RG", // Amazon Item
+    ITEM_PRICE: "$499.99", //We use the price to check if the item is available. Format: $xx.xx
+    MSG: "PS5 IN-STOCK! https://www.amazon.com/dp/B08FC5L3RG" // Message to be sent in text message
+}
---
+++ index.js
@@ -0,0 +1,46 @@
+var Product  = require('amazonproductscraper').Product;
+const {SID, TOKEN, TO_PHONES, FROM, ITEM, ITEM_PRICE, MSG}  = require('./config');
+
+
+var sendAlert = function(sendTo) {
+    var client = require('twilio')(SID, TOKEN),
+        body   = MSG;
+
+    client.messages
+        .create({
+            to: sendTo,
+            body: body,
+            from: FROM
+        })
+        .then(function() {
+            console.log('Text alert sent to: ' + sendTo);
+        }).done();
+};
+
+async function checkAvailability() {
+    var item = await new Product(ITEM).init();
+    var time = new Date();
+
+    TO_PHONES.forEach(p => console.log(p));
+
+
+    console.log("---------------------------------------------------")
+    console.log("Time Stamp:",
+        time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+    );
+    console.log("PRODUCT: ", item.getTitle());
+    console.log("AVAILABILITY: ", item.getPrice());
+    var isProductAvailable = item.getPrice() === ITEM_PRICE;
+
+    if (isProductAvailable) {
+        console.log("ITEM IS IN-STOCK!", item.getPrice());
+        TO_PHONES.forEach(p => sendAlert(p));
+    } else {
+        console.log('Product Not Available, checking again in 1m');
+        setTimeout(function() {
+            checkAvailability();
+        }, 60000);
+    }
+}
+
+checkAvailability();
